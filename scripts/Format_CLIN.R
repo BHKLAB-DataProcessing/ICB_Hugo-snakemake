7e0b6b64f7a961e4789acfce42bc551fe543ecc9
Minoru Nakano <minoru.nakano@gmail.com>
2022-07-08 16:38:46 -0400

    update clin processing

--- scripts/Format_CLIN.R
+++ scripts/Format_CLIN.R
@@ -3,10 +3,11 @@ input_dir <- args[1]
 output_dir <- args[2]
 
 source("https://raw.githubusercontent.com/BHKLAB-Pachyderm/ICB_Common/main/code/Get_Response.R")
+source("https://raw.githubusercontent.com/BHKLAB-Pachyderm/ICB_Common/main/code/format_clin_data.R")
 
-clin = read.csv( file.path(input_dir, "CLIN.txt"), stringsAsFactors=FALSE , sep="\t" , dec=',')
-
-clin = cbind( clin[ , c( "Patient.ID","irRECIST","Gender","Age","Overall.Survival","Vital.Status" ) ] , "PD-1/PD-L1" , "Melanoma" , NA , NA , NA , NA , NA , NA , NA , NA )
+clin_original = read.csv( file.path(input_dir, "CLIN.txt"), stringsAsFactors=FALSE , sep="\t" , dec=',')
+selected_cols <- c( "Patient.ID","irRECIST","Gender","Age","Overall.Survival","Vital.Status" )
+clin = cbind( clin_original[ , selected_cols] , "PD-1/PD-L1" , "Melanoma" , NA , NA , NA , NA , NA , NA , NA , NA )
 colnames(clin) = c( "patient" , "recist" , "sex"  ,"age"  , "t.os" , "os" , "drug_type" , "primary" , "response.other.info" , "response" , "histo" , "stage" , "t.pfs" , "pfs" , "dna" , "rna")
 
 clin$t.os = clin$t.os / 30.5
@@ -23,6 +24,8 @@ clin$rna[ clin$patient %in% case[ case$expr %in% 1 , ]$patient ] = "fpkm"
 clin$dna[ clin$patient %in% case[ case$cna %in% 1 , ]$patient ] = "wes"
 
 clin = clin[ , c("patient" , "sex" , "age" , "primary" , "histo" , "stage" , "response.other.info" , "recist" , "response" , "drug_type" , "dna" , "rna" , "t.pfs" , "pfs" , "t.os" , "os" ) ]
+
+clin = format_clin_data(clin_original, 'Patient.ID', selected_cols, clin)
 
 write.table( clin , file=file.path(output_dir, "CLIN.csv") , quote=FALSE , sep=";" , col.names=TRUE , row.names=FALSE )
 
